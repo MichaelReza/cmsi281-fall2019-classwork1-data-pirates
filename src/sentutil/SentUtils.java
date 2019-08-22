11b9134a09e5a22be9708a2ba5d8124634dcc737
Forns <forneyandrew@gmail.com>
2019-08-21 21:58:58 -0700

    Initial Skeleton for CW 1 Assignment!

---
+++ src/sentutil/SentUtils.java
@@ -0,0 +1,19 @@
+package sentutil;
+
+/**
+ * Simple library which might (outside of this assignment) contain
+ * various functions related to some sentence utilities.
+ */
+public class SentUtils {
+
+    /**
+     * Returns the number of unique, repeated words that are found
+     * in the given sentence sent
+     * @param sent The sentence in which to count repeated words
+     * @return The number of unique, repeated words
+     */
+    public static int repeats (String sent) {
+        throw new UnsupportedOperationException();
+    }
+
+}
